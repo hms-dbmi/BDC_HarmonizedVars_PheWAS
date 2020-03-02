457420c82746758196e48c6fc08a2858a4795010
Ubuntu <ubuntu@ip-172-31-46-224.ec2.internal>
2020-03-02 18:00:25 +0000

    results

--- python/studies_stats.sh
+++ python/studies_stats.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
-INPUT=../studies_info.csv
-CORES=`cat /proc/cpuinfo | grep processor | wc -l`
-JOBS=`ps -df | grep python | wc -l`
-OLDIFS=$IFS
-IFS=','
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read phs BDC_study_name official_study_name short_name harmonized phs_list ID_varName
-do
-	while [ $[$CORES + 1] -le $JOBS ]; do
-		sleep 2
-		JOBS=`ps -df | grep python | wc -l`
-	done
-	python3.6 studies_stats.py --phs=$phs > results/log_$phs.txt &
+#INPUT=../studies_info.csv
+INPUT=phs_list.txt
+#CORES=`cat /proc/cpuinfo | grep processor | wc -l`
+CORES=8
+JOBS=`ps -df | grep studies_stats | wc -l`
+echo "jobs at the beginning $JOBS"
+echo "cores at the beginning: $CORES"
+#OLDIFS=$IFS
+#IFS=','
+#[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+#while read phs BDC_study_name official_study_name short_name harmonized phs_list ID_varName
+while IFS= read -r phs; do
+        JOBS=`ps -df | grep studies_stats | wc -l`
+        while [ "$[$CORES + 1]" -le "$JOBS" ]; do
+                echo "cores loop $CORES"
+                echo "jobs loop $JOBS"
+                sleep 2
+                JOBS=`ps -df | grep studies_stats | wc -l`
+        done
+        echo "launching process: $phs"
+        nohup python3.6 studies_stats.py --phs=$phs > results/log_$phs.txt &
 done < $INPUT
-IFS=$OLDIFS
+#IFS=$OLDIFS
+
