c67e0aca91ded05e607b83904e954354b2a3c3cf
Arnaud Serret-Larmande <arnaudsl12@gmail.com>
2020-03-03 18:22:34 -0500

    new chnages

--- python/run_PheWAS.py
+++ python/run_PheWAS.py
@@ -25,13 +25,22 @@ print("getting resource")
 resource = get_HPDS_connection(token,
                                "https://picsure.biodatacatalyst.nhlbi.nih.gov/picsure",
                                "02e23f52-f354-4e8b-992c-d37c8b9ba140")
-dependent_var_name = "\\DCC Harmonized data set\\01 - Demographics\\Subject sex  as recorded by the study.\\"
-print("entering PheWAS")
-dic_pvalues, dic_errors = PheWAS(study_variables, dependent_var_name, resource)
-print("PheWAS done")
+
+harmonized_var_names = pd.read_csv("studies_stats/harmonized_details_stats.csv", index_col=0).index.tolist()
+dependent_var_names = [harmonized_var_names[i] for i in [4, 22, 58]]
+
+big_dic_pvalues = {} 
+big_dic_errors = {}
+for dependent_var_name in dependent_var_names:
+    print("entering PheWAS")
+    dic_pvalues, dic_errors = PheWAS(study_variables, dependent_var_name, resource)
+    print("PheWAS done")
+    big_dic_pvalues[dependent_var_name] = dic_pvalues
+    big_dic_errors[dependent_var_name] = dic_errors
+
 
 with open(os.path.join("results/pvalues", phs + "_pvalues.json"), "w+") as f:
-    json.dump(dic_pvalues, f)
+    json.dump(big_dic_pvalues, f)
 
 with open(os.path.join("results/pvalues", phs + "_errors.json"), "w+") as f:
-    json.dump(dic_errors, f)
+    json.dump(big_dic_errors, f)
