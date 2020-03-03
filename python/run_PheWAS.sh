7770834437c76c80cf1571ccb40c6319ed47b6eb
Arnaud Serret-Larmande <arnaudsl12@gmail.com>
2020-03-03 01:32:23 -0500

    phewas script

--- python/run_PheWAS.sh
+++ python/run_PheWAS.sh
@@ -2,23 +2,27 @@
 INPUT=phs_list.txt
 #CORES=`cat /proc/cpuinfo | grep processor | wc -l`
 CORES=8
-JOBS=`ps -df | grep studies_stats | wc -l`
+JOBS=`ps -df | grep run_PheWAS | wc -l`
 echo "jobs at the beginning $JOBS"
 echo "cores at the beginning: $CORES"
 #OLDIFS=$IFS
 #IFS=','
 #[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 #while read phs BDC_study_name official_study_name short_name harmonized phs_list ID_varName
-while IFS= read -r phs; do
-        JOBS=`ps -df | grep studies_stats | wc -l`
+
+for f in $(ls ./studies_stats | grep "phs.*csv");
+do
+     phs=${f::9}
+     JOBS=`ps -df | grep run_PheWAS | wc -l`
         while [ "$[$CORES + 1]" -le "$JOBS" ]; do
-                echo "cores loop $CORES"
-                echo "jobs loop $JOBS"
+                #echo "cores loop $CORES"
+                #echo "jobs loop $JOBS"
                 sleep 2
-                JOBS=`ps -df | grep studies_stats | wc -l`
+                JOBS=`ps -df | grep run_PheWAS | wc -l`
         done
+        #echo $phs
         echo "launching process: $phs"
-        nohup python3.6 studies_stats.py --phs=$phs > results/log_$phs.txt &
+        nohup python run_PheWAS.py --phs=$phs > logs/log_PheWAS_$phs.txt &
 done < $INPUT
 #IFS=$OLDIFS
 
