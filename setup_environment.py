e1dbc3a7ec4df99387b054bfb8c3a63cae6ec0e7
Arnaud Serret-Larmande <arnaudsl12@gmail.com>
2021-05-05 11:08:36 -0400

    writing list_harmonized_variables

--- setup_environment.py
+++ setup_environment.py
@@ -1,5 +1,7 @@
 import yaml
 
+import pandas as pd
+
 from env_variables.env_variables import TOKEN,\
     RESOURCE_ID,\
     PICSURE_NETWORK_URL,\
@@ -21,16 +23,28 @@ subset_variables_dictionary = variables_dictionary\
 subset_variables_dictionary = subset_variables_dictionary.groupby("level_0") \
     .apply(lambda df: get_batch_groups(df, batch_size, assign=True))
 
+
 subset_variables_dictionary.to_csv("env_variables/multiIndex_variablesDict.csv")
 subset_variables_dictionary.reset_index("level_0", drop=False)\
     .rename({"level_0": "study"}, axis=1)\
     .loc[:, ["study", "phs", "batch_group", "name"]]\
     .to_csv("env_variables/list_eligible_variables.csv", index=False)
 
+renaming_harmonized_variables_manual = pd.read_csv("env_variables/renaming_harmonized_variables_manual.csv")\
+    .loc[lambda df: df["renaming_variables"].notnull(), :]\
+    .set_index("harmonized_complete_name")
+
+variables_dictionary.join(renaming_harmonized_variables_manual,
+                          on="name",
+                          how="inner")\
+    .loc[:, ["name", "renaming_variables", "renaming_variables_nice", "categorical"]]\
+    .reset_index(drop=True)\
+    .to_csv("env_variables/list_harmonized_variables.csv", index=False)
+
 parameters = {
     "univariate": True,
     "Minimum number observations": 5,
-    "variable_types": "categorical"
+    "harmonized_variable_types": "categorical"
 }
 with open("env_variables/parameters_exp.yaml", "w+") as f:
     yaml.dump(parameters, f)
